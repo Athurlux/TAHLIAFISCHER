37a7a8da8aad24ae6c6a2bb4839a36f3eca0d847
Athurlux <149949122+Athurlux@users.noreply.github.com>
2026-02-27 12:29:56 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // <--- This is required for Cloudflare Static Hosting
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true, // <--- This is required for 'export' mode
     remotePatterns: [
       {
         protocol: 'https',
